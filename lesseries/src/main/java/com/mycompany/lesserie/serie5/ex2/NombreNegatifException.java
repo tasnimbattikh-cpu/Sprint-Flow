2becf19ce88288d4c5f317355a9f68ec90fe5eab
tasnimbattikh-cpu <tasnim.battikh@etudiant-fsegt.utm.tn>
2026-04-23 15:23:54 +0100

    Delete lesseries directory

--- lesseries/src/main/java/com/mycompany/lesserie/serie5/ex2/NombreNegatifException.java
+++
@@ -1,9 +0,0 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.lesserie.serie5.ex2;
-
-public class NombreNegatifException extends Exception {
-    
-}
